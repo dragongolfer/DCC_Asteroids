7108d5f9a3156ee6514a4a1fd2a8431a762cec85
mkohlmann-he <mkohlmann-he@dev.bse.com>
2015-06-17 14:01:53 -0500

    Cleaned up code after Hit-Hell.

--- engine.py
+++ engine.py
@@ -19,9 +19,10 @@ from random import *
 import math
 pygame.init()
 
-from ship import *             # Ryan
+from ship import *              # Ryan
 # from AsteroidsFTW import *      # Rob
-from display_functions import *           # Matt
+from display_functions import * # Matt
+from bulletClass import *       #Mike
 
 def debug(message):
     DEBUG = True
@@ -29,45 +30,7 @@ def debug(message):
         print("DEBUG: " + str(message))
     return
     
-   
 
-##### Debugging Section Stub-Out (IGNORE THIS, THIS IS JUST TO PREVENT ERRORS WHILE DEBUGGING)
-##### WILL BE REPLACED WITH ACTUAL CLASSES
-# class Ship:
-#     def __init__(self):
-#         self.x = 400
-#         self.y = 400
-#         self.angle = randint(0,360)
-#         self.speed = 1
-#         return
-
-#     def processUserInputs(self):
-#         pass
-#         return
-        
-#     def ProcessTimeIncrement(self):
-#         self.x += self.speed * math.cos(math.radians(self.angle))
-#         self.y -= self.speed * math.sin(math.radians(self.angle))
-#         if self.x > 800:
-#             self.x -= 800
-#         if self.x < 0:
-#             self.x += 800
-#         if self.y > 800:
-#             self.y -= 800
-#         if self.y < 0:
-#             self.y += 800
-#         return
-        
-#     def checkCollision(self, location, size):
-#         pass
-#         return
-
-#     def get_position(self):
-#         pos = (self.x, self.y)
-#         return pos
-
-#     def get_angle(self):
-#         return self.angle
         
 class Asteroid(pygame.sprite.Sprite):
     def __init__(self):
@@ -99,69 +62,32 @@ class Asteroid(pygame.sprite.Sprite):
         return
 
 
-'''
-def displayGameScreen(objectList, screen):
-    backgroundimage = pygame.image.load("StarField.gif")
-    background = pygame.transform.scale(backgroundimage,(width,height))
-    screen.blit(background, (0,0))
-    
-    ship = objectList[1]
-    shipImage = pygame.image.load("ship.png")
-    shipLocation = ship.getLocation()
-    screen.blit(shipImage, shipLocation)
-    
-    asteroidImage = pygame.image.load("Asteroid.gif")
-    for each in objectList[1:]:
-        asteroidLocation = each.getLocation()
-        screen.blit(asteroidImage,asteroidLocation)
-
-    pygame.display.flip()
-    
-    return
-'''
-
 # Single Round of the main game loop    
 def main(objectList, screen, lives,size):
     endOfRound = False
     clock = pygame.time.Clock()
     while not endOfRound:
+        # 60 Framse per second
+        clock.tick(60) # Game will render at 60 frames per second
+    
+        # Process User Inputs
         ship = objectList[0]
         for event in pygame.event.get():#user does something
             if event.type == pygame.QUIT:
                 endOfRound = True
             elif event.type == pygame.KEYDOWN:
                 ship.keydown(event)
-                #ship.keydown(event,objectList[0])
             elif event.type == pygame.KEYUP:
                 ship.keyup(event)
-                #keyup(event,objectList[0])
-        clock.tick(60) # Game will render at 60 frames per second
-        timeStamp = time.clock()
-        debug("TIME is: " + str(timeStamp))
-        # Read User Inputs
-        debug("")
-        #ship = objectList[0]
-
-        #ship.processUserInputs()
 
         # Process Time Increment
         debug("STARTING PROCESS TIME INCREMENT")
         for each in objectList:
-            each.ProcessTimeIncrement(size)
-
+            each.update(size)
 
         # Process Collision Detect
         debug("STARTING COLLISION DETECT")
-        asteroid_list = list(objectList[1:])
-        #for each in objectList[1:]:
-            #Asteroid_Center_Location = each.get_location()
-            #Asteroid_Size = each.getSize()
-            #for asteroid in asteroid_list:
-            #collisionDetected = ship.checkCollision(Asteroid_Center_Location, Asteroid_Size)
-            #if collisionDetected:
-                #lives -= 1
-                # Reset 
-                #break
+        ###### Add Collision detect here.
 
         # Process Graphics
         debug("STARTING GRAPHICS ENGINE")
